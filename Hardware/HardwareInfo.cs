3da7a215d4e056acd62878ad292cc83d44b3dc7b
githubchik228 <144711603+githubchik228@users.noreply.github.com>
2026-08-11 19:05:59 +0500

    Add HardwareInfo class to manage hardware details

---
+++ Hardware/HardwareInfo.cs
@@ -0,0 +1,8 @@
+namespace RustFPSOptimizer.Hardware;
+public class HardwareInfo
+{
+    public string Cpu { get; set; } = "Unknown";
+    public string Gpu { get; set; } = "Unknown";
+    public string Ram { get; set; } = "Unknown";
+    public string OperatingSystem { get; set; } = "Unknown";
+}

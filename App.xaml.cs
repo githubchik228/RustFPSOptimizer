9f7c8c08eb75536287f7c441474651dcad5644db
githubchik228 <144711603+githubchik228@users.noreply.github.com>
2026-08-11 18:57:52 +0500

    Override OnStartup method in App class

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,4 +2,8 @@ using System.Windows;
 namespace RustFPSOptimizer;
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+    }
 }
